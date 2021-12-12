1607aaf016cf81c3ba213cf8b1b4c709a7ff558b
Joshua Cruz <60358264+polar1s7@users.noreply.github.com>
2021-12-12 13:25:58 +0800

    Update script.js
    Just cleaned something.

--- html/js/script.js
+++ html/js/script.js
@@ -3,7 +3,7 @@ window.addEventListener('message', function(event){
         document.getElementById("text").innerHTML = event.data.text;
         document.getElementById("container").classList.remove("fadeOut");
         document.getElementById("container").classList.add("fadeIn");
-        document.documentElement.style.setProperty("--selection-background", "DarkTurquoise "); /* default blue */
+        document.documentElement.style.setProperty("--selection-background", "DarkTurquoise"); /* default blue */ 
         if(event.data.color != undefined){
             document.documentElement.style.setProperty("--selection-background", event.data.color);
         }
